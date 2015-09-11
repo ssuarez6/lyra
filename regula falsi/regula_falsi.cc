ada23899e1873a2f8bd19202d3787658fe0000ba
Santiago S.P <suarez.santiago.perez@gmail.com>
2015-09-11 11:30:10 -0500

    adding files for regula falsi

---
+++ regula falsi/regula_falsi.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long double f(long double x);
+
+int main(){
+  cout << "Give me a range you know there's a root for sin(x), then tolerance, and then iterations" << endl;
+  long double xi=0,xs=0,tol=0,iter=0,yi=0,ys=0;
+  yi = f(xi);
+  ys = f(xs);
+  cin >> xi >> xs >> tol >> iter;
+  if(yi*ys==0){
+    cout << "Roots are equals" << endl;
+    return 0;
+  }else if(yi==0){
+    cout << xi << " is a root" << endl;
+    return 0;
+  }else if(ys==0){
+    cout << xs << " is a root" << endl;
+    return 0;
+  }else{
+    long double xm = (xi+xs) / 2;
+    long double ym = f(xm);
+    long double error = tol * 2;
+    long double cont = 1;
+    while(ym!=0 and error>tol and cont <= iter){
+      if(ym*yi == 0){
+	xs = xm;
+	ys = ym;
+      }else{
+	xi = xm;
+	yi = ym;
+      }
+      long double aux = xm;
+      xm = (yi*xs - xi*ys) / (yi-ys);
+      error = abs(xm-aux);
+      cont++;
+    }
+    if(ym==0){
+      cout << xm << " is a root" << endl;
+      return 0;
+    }else if(error<tol){
+      cout << xm << " is a root. Error=" << error << endl;
+    }else if(cont>iter){
+      cout << "iterations over. root not found" << endl;
+    }
+  }
+  return 0;
+}
+
+long double f(long double x){
+  return sin(x);
+}
