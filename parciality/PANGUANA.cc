9efa930c88cebe7ff18283f73ae818f01904cbb8
Santiago S.P <suarez.santiago.perez@gmail.com>
2015-09-18 15:37:12 -0500

    Adding the file dor the newton method, missing the g(x) implementation

--- parciality/PANGUANA.cc
+++ parciality/PANGUANA.cc
@@ -1,23 +1,24 @@
 #include "PANGUANA_header.hpp"
 #include <iostream>
+
 using namespace std;
 
-parser_panguana::parser_panguana(){
-  cout << "Mostro, la expresion\nf(x)=";
-  string e;
-  cin >> e;
-  asignar_expresion(e);
+parser_panguana::parser_panguana() {
+    cout << "Mostro, la expresion\nf(x)=";
+    string e;
+    cin >> e;
+    asignar_expresion(e);
 }
 
-void parser_panguana::asignar_expresion(string panguana_de_expr){
-  cout << "Esto es una panguana haciendo panguana-cosas" << endl;
-  st.add_variable("x", x);
-  st.add_constants();
-  expression.register_symbol_table(st);
-  parser.compile(panguana_de_expr, expression);
+void parser_panguana::asignar_expresion(string panguana_de_expr) {
+    cout << "Esto es una panguana haciendo panguana-cosas" << endl;
+    st.add_variable("x", x);
+    st.add_constants();
+    expression.register_symbol_table(st);
+    parser.compile(panguana_de_expr, expression);
 }
 
-long double parser_panguana::evaluar_en(long double param){
-  x = param;
-  return expression.value();
+long double parser_panguana::evaluar_en(long double param) {
+    x = param;
+    return expression.value();
 }
