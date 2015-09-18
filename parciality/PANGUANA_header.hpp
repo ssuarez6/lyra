bcbf6ea8f00c27a69f5280a08ddd39f6a92dc9f8
Santiago S.P <suarez.santiago.perez@gmail.com>
2015-09-18 14:20:10 -0500

    Files for the PARCIALITYgit add . -A

---
+++ parciality/PANGUANA_header.hpp
@@ -0,0 +1,14 @@
+#include <string>
+#include "exprtk.hpp"
+class parser_panguana {
+private:
+  long double x;
+  std::string le_expresie;
+  exprtk::symbol_table<long double> st;
+  exprtk::expression<long double> expression;
+  exprtk::parser<long double> parser;
+public:  
+  parser_panguana();
+  void asignar_expresion(std::string panguana_de_expr);
+  long double evaluar_en(long double param);  
+}; 
