70de2a2128e302829272761c9181030c52b5ec05
Santiago S.P <suarez.santiago.perez@gmail.com>
2015-10-05 16:43:44 -0500

    plotting python

---
+++ python/plot.py
@@ -0,0 +1,56 @@
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+
+replacements = {
+    'sin' : 'np.sin',
+    'cos' : 'np.cos',
+    'exp': 'np.exp',
+    'sqrt': 'np.sqrt',
+    '^': '**',
+    'tan': 'np.tan',
+    'arctan': 'np.arctan',
+    'arcsin': 'np.arcsin',
+    'arccos': 'np.arccos',
+}
+
+allowed_words = [
+    'x',
+    'sin',
+    'cos',
+    'sqrt',
+    'exp',
+    'tan',
+    'arctan',
+    'arcsin',
+    'arccos',
+]
+
+def string2func(string):
+    ''' evaluates the string and returns a function of x '''
+    # find all words and check if all are allowed:
+    for word in re.findall('[a-zA-Z_]+', string):
+        if word not in allowed_words:
+            raise ValueError(
+                '"{}" is forbidden to use in math expression'.format(word)
+            )
+
+    for old, new in replacements.items():
+        string = string.replace(old, new)
+
+    def func(x):
+        return eval(string)
+
+    return func
+
+def graficar(f):
+    func = string2func(f)
+    x = np.linspace(-10, 10, 250)
+    plt.plot(x, func(x))
+    plt.xlim(-10, 10)
+    plt.show()
+
+
+if __name__ == '__main__':
+    s = raw_input('Enter a function\nf(x) = ')
+    graficar(s)
