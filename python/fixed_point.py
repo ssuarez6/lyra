967157090eb8be9bf74ee4e763a72de986cb87cf
Daniel Velásquez <dvelas26@gmail.com>
2015-10-06 23:09:14 -0500

    Add the secant and newton method,errors in the column f_(x), that affect the anlysis but not the answer

--- python/fixed_point.py
+++ python/fixed_point.py
@@ -7,25 +7,32 @@ tolerance=float(input("Type the tolerance "))
 xaproximmate=float(input("Type the initial approximation "))
 iterations=(input("Type the number of iterations "))
 function=raw_input("Type the f function ")
-functiong=raw_input("Type the g function ")
+functiong=raw_input("Type the g function \n")
 function_evaluated=float(parser.eval(function,xaproximmate))
 functiong_evaluated=float(parser.eval(functiong,xaproximmate))
 error=tolerance+1
 contador=(0)
 
+rows=[]
 while function_evaluated!=0 and error > tolerance and contador < iterations :
             x_n          = functiong_evaluated
             x_n_evaluated= float(parser.eval(functiong,x_n))
-            f_x_n        = float(parser.eval(function, functiong_evaluated))
             xaproximmate = x_n
             error        = abs(x_n_evaluated-x_n)
+            f_x_n_evaluated= float(parser.eval(function,x_n_evaluated))
+            v=(contador,x_n,f_x_n_evaluated,error)
+            rows.append(v)
             contador     = contador+1
             functiong_evaluated    = x_n_evaluated
-print iterations
+            t = Table(rows=rows, names=('iter', 'x_n', 'f_x_n','error'))
+
+
 if function_evaluated==0:
     print "X_a es raiz"
 elif error < tolerance:
     print str(xaproximmate)+ " es aproximacion con una tolerancia " +str(tolerance)+" hallado en la iteracion "+ str(contador) + " y un error de " +str(error)
 else:
     print "El metodo fracaso en "+str(iterations)+" iteraciones"
+
+print t
 plot.graficar(function)
