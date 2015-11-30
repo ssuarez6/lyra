7b021f92fc56b6c58a2947322628ad1fd5fdbedb
Santiago S.P <suarez.santiago.perez@gmail.com>
2015-11-30 01:57:19 -0500

    jacoby and gaussseidel with relaxation

--- equations_systems/Jacobi.py
+++ equations_systems/Jacobi.py
@@ -1,55 +1,32 @@
-import numpy as np
+from __future__ import division
 from numpy import array, zeros, diag, diagflat, dot
+import numpy as np
 
-#tol=input("Digite la tolerancia \n")
-#niter=input("Digite el numero de iteraciones \n")
-contador=0
-#dispersion=tol+1
-def leeMat(numberofColumns, numberofRows):
-    ax=np.zeros((numberofRows,numberofColumns)) #Creo una matriz del size que ingreso el usuario
-                                                               # y la lleno de zeros
-    #Ciclo que llena la matriz
-    for i in range(numberofRows):
-        for j in range(numberofColumns):
-            ax[i][j]= input("Type the term number "+ str(j+1)+ " in the equation number "+ str(i+1)+ " "+"\n")
+"""Solves the equation Ax=b via the Jacobi iterative method."""
+"""A: Coefficient, b: solution, n: iterations, x: initial values, tol: tolerance, l: lambda""" 
+def jacobi(A,b,n,x,tol,l):
+	it_count = 0
+	error = tol+1
+	while it_count<n:
+		print "Current solution:", x
+		print "Current error:", error
+		x_new = np.zeros_like(x)
+		i = 0
+		for i in range(A.shape[0]):
+			s1 = np.dot(A[i, :i], x[:i])
+			s2 = np.dot(A[i, i + 1:], x[i + 1:])
+			x_new[i] = np.divide((b[i]-s1-s2), A[i,i])*l + (1-l)*x[i]
+		error = np.amax(np.dot(A,x) - b)	
+		x = x_new
+		it_count += 1
+	print "Solution: "
+	print x
 
-    return ax
-
-def leeSol(nEquations):
-    solutionColumns=1
-    solutionVector=np.zeros((solutionColumns,nEquations))
-    #Ciclo que llena la columna solucion
-    for k in range(nEquations):
-        for l in range(solutionColumns):
-            solutionVector[k][l]  = input("Type the solution for equation "+str(k+1)+" "+"\n")
-
-    return solutionVector
-
-
-
-A=leeMat(input("Type the number of terms \n "),input("Type the number of equations in your system  \n"))
-
-x=initialValues(input("Type the initial values \n"))
-n=input("Type the number of iterations")
-
-
-def jacobi(A,b,n,x):
-    """Solves the equation Ax=b via the Jacobi iterative method."""
-    # Create an initial guess if needed
-    if x is None:
-        x = zeros(len(A[0]))
-
-    # Create a vector of the diagonal elements of A
-    # and subtract them from A
-    D = diag(A)
-    R = A - diagflat(D)
-
-    # Iterate for N times
-    for i in range(N):
-        x = (b - dot(R,x)) / D
-    return x
-
-
-
-
-print "all good"
+if __name__ == '__main__':
+	A = np.array([[10.0, -1.0, 2.0, 0.0], [-1.0,11.0,-1.0,3.0],[2.0,-1.0,10.0,-1.0],[0.0, 3.0, -1.0, 8.0]])
+	b = np.array([6.0,25.0,-11.0,15.0])
+	x0 = np.array([0.0,0.0,0.0,0.0])
+	_iter = 10
+	err = 1e-8
+	l  = 1.0
+	jacobi(A, b, _iter, x0, err, l)
