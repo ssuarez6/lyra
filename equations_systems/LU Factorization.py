c8d4a5b3e0524c45eb7a0fc43fd8cd1cf4800969
Daniel Velásquez <dvelas26@gmail.com>
2015-12-01 10:13:25 -0500

    Fixed again LU

--- equations_systems/LU Factorization.py
+++ equations_systems/LU Factorization.py
@@ -2,7 +2,7 @@ __author__ = 'parzival'
 #from methods import eval
 import scipy
 import scipy.linalg
-def PLU(A,B):
-	P, L, U = scipy.linalg.lu(A.get('matriz'))
+def PLU(A,):
+	P, L, U = scipy.linalg.lu(A)
 	return{'status': "SUCESS", 'message':"l"+str(L)+"u"+str(U)+"p"+str(P)}
 
